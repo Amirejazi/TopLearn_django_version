1d0929db8d529a5098d586584cc9cc80bdd8a765
Amirhosein.e <ejaziamirhosein@gmail.com>
2023-10-06 04:30:52 +0330

    handle 404 error

---
+++ middlewares/handle404_middleware.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseRedirect
+
+
+class Handle404Middleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if response.status_code == 404:
+            return HttpResponseRedirect('/error404')
+        return response
