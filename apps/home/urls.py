1f73aca1cf55099b42bae4dae009e3fca44a2349
Amirhosein.e <ejaziamirhosein@gmail.com>
2023-10-06 04:32:35 +0330

    add popular & latest courses in index with redis catch

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -4,4 +4,8 @@ from .views import *
 app_name = 'home'
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('error404', Error404.as_view(), name='Error404'),
+    path('latest_courses/', latest_course, name='LatestCourses'),
+    path('popular_courses', popular_courses, name='PopularCourses'),
+    path('search_complete', search_complete, name='SearchComplete'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,8 +1,10 @@
+import json
 from django.conf import settings
 from django.db.models import Sum
+from django.http import HttpResponseNotFound, JsonResponse, HttpResponse
 from django.shortcuts import render
 from django.views import View
-
+from django.views.generic import TemplateView
 from apps.course.models import Course
 
 
@@ -12,5 +14,29 @@ def media_admin(request):
 
 class Index(View):
     def get(self, request):
-        latest_courses = Course.objects.order_by('createDate').annotate(total_time=Sum('episodes__episodeTime'))[:8]
-        return render(request, 'Home_app/index.html', {'latest_courses': latest_courses})
+        return render(request, 'Home_app/index.html')
+
+
+class Error404(TemplateView):
+    template_name = "Home_app/Error404.html"
+
+
+def latest_course(request):
+    latest_courses = Course.objects.order_by('-createDate').annotate(total_time=Sum('episodes__episodeTime'))[:8]
+    return render(request, 'Course_app/render_partials/LatestCourse.html', {'latest_courses': latest_courses})
+
+
+def popular_courses(request):
+    popular_course = Course.objects.annotate(order_detail_count=Sum('order_details__count')) \
+                         .order_by('-order_detail_count') \
+                         .annotate(total_time=Sum('episodes__episodeTime'))[:8]
+    return render(request, 'Course_app/render_partials/PopularCourse.html', {'popular_courses': popular_course})
+
+
+def search_complete(request):
+    term = request.GET.get('term')
+    if term is not None:
+        # چون میخواهیم خروجیمان بصورت لیست باشد falt را true میگذاریم
+        course_titles = Course.objects.filter(courseTitle__icontains=term).values_list('courseTitle', flat=True)
+        return HttpResponse(json.dumps(list(course_titles), ensure_ascii=False), content_type="application/json")
+    return HttpResponseNotFound()
